b2b557d82e4e6152e8101d8944e5aa417754e7fb
Wissam Taleb <wissam@turnkey-sys.com>
2019-06-03 12:14:01 +0300

    sdfsdf

--- src/index.js
+++ src/index.js
@@ -4,14 +4,34 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import axios from 'axios';
+import * as jwt_decode from 'jwt-decode';
+import Cookies from 'universal-cookie';
 
 
+let cookie = new Cookies();
 axios.interceptors.request.use(request => {
+
+    if(cookie.get("id_token")){
+
+        request.headers.Authorization = `Bearer ${cookie.get("id_token")}`;
+    }
+    
     console.log(request);
+
+    
     return request;
 })
 
 axios.interceptors.response.use(response => {
+
+    if(response.headers.get("token")){
+        const token = response.headers.get("token");
+        cookie.set("id_token",token);
+        const tokenInfo = jwt_decode(token);
+        const expireDate = tokenInfo.exp;
+        cookie.set("expires_at",  JSON.stringify(expireDate.valueOf())  );
+
+    }
     console.log(response);
     return response;
     
