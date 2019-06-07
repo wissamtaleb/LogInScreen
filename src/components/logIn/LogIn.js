5de5851b1dc15f065a3a8be038ef6ae8a0773723
Wissam Taleb <wissam@turnkey-sys.com>
2019-06-07 15:21:19 +0300

    added validators

--- src/components/logIn/LogIn.js
+++ src/components/logIn/LogIn.js
@@ -7,7 +7,10 @@ class LogIn extends Component{
 
     state = {
         username: '',
-        password: ''
+        password: '',
+        usernameError: '',
+        passwordError: '',
+        loading: false
     }
 
     constructor(){
@@ -18,25 +21,67 @@ class LogIn extends Component{
     handleChange(e){
 
         this.setState({
-            [e.target.name]: e.target.value
+            [e.target.name]: e.target.value,
+            
         })
+    }
+
+    validateUsername(){
+        if(this.state.username === ''){
+            this.setState({
+                usernameError: "USERNAME is required"
+            })
+        }
+        else{
+            this.setState({
+                usernameError: ''
+            })
+        }
+    }
+
+    validatePassword(){
+        if(this.state.password === ''){
+            this.setState({
+                passwordError: "PASSWORD is required"
+            })
+        }
+        else{
+            this.setState({
+                passwordError: ''
+            })
+        }
     }
 
     handleSubmit(e){
 
         e.preventDefault();
-        let username = this.state.username;
-        let password = this.state.password;
-
-        let loginCredentials = {
-            username: username,
-            password: password
-        }
-        axios.post('http://localhost:8080/Quiz_app-0.0.1-SNAPSHOT/index/logIn', loginCredentials).then(response => {
-             console.log(response.data);
+        this.setState({
+            loading: true
         })
 
-    }
+        var self = this;
+        setTimeout(function(){
+            self.setState({
+
+                loading: false,
+            })
+           self.validateUsername();
+            self.validatePassword();
+            if(self.state.usernameError === '' && self.state.passwordError ===''){
+            let username = self.state.username;
+            let password = self.state.password;
+    
+            let loginCredentials = {
+                username: username,
+                password: password
+            }
+            axios.post('http://localhost:8080/Quiz_app-0.0.1-SNAPSHOT/index/logIn', loginCredentials).then(response => {
+                 console.log(response.data);
+            })
+    
+        } },1000)
+     
+}
     
     render(){
         return(<div>
@@ -58,9 +103,9 @@ class LogIn extends Component{
             {/* <app-inline-notification className="notification float-none" [notifications]="getnotifications()" (OnNotificationRemoved)="onNotificationRemoved($event)"> </app-inline-notification> */}
             <div className="form-group">
                 
-                <input placeholder="USERNAME"   name="username" type="text" value = {this.state.username} onChange = {(e) => this.handleChange(e)}  className="form-control" id="username"/>
+                <input placeholder="USERNAME"   error = "usernameError" name="username" type="text" value = {this.state.username} onChange = {(e) => this.handleChange(e)}  className="form-control" id="username"/>
                 <div >
-                    <small  className="text-danger">USERNAME is required</small>
+                    <small  className="text-danger">{this.state.usernameError}</small>
                 </div>
 
             </div>
@@ -68,14 +113,14 @@ class LogIn extends Component{
                 
                 <input placeholder="PASSWORD"   value = {this.state.password} onChange = {(e) => this.handleChange(e)} name="password" type="password" className="form-control" id="password"/>
                 <div >
-                    <small  className="text-danger">PASSWORD is required</small>
+                    <small  className="text-danger">{this.state.passwordError}</small>
                 </div>
             </div>
             <div className="button-group">
                 <div className="float-left">
                     <button   className="btn btn-danger float-left" onClick={(e)=> this.handleSubmit(e)} type="submit">Login</button>
-                    <img  src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA=="
-                    />
+                    {this.state.loading && <img  src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA=="
+                    />}
                 </div>
                 <div className="float-right">
                     <button className="btn btn-light" >Forget Password
